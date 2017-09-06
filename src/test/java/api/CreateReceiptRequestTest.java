e2d6583d47988fb2fa0df5b633579cbd3817e66e
Zhiyuan Jia <jzyjune@Zhiyuans-MacBook-Pro.local>
2017-09-05 23:48:20 -0400

    fix2

--- src/test/java/api/CreateReceiptRequestTest.java
+++ src/test/java/api/CreateReceiptRequestTest.java
@@ -28,11 +28,7 @@ public class CreateReceiptRequestTest {
     public void testMissingAmount() {
         CreateReceiptRequest receipt = new CreateReceiptRequest();
         receipt.merchant = "OK";
-<<<<<<< HEAD
-=======
 
->>>>>>> b07c2f917ba21e931340a595ba355e5ac0b57f81
-        //receipt.amount = new BigDecimal(33.44);
         assertThat(validator.validate(receipt), empty());
     }
 
@@ -44,7 +40,6 @@ public class CreateReceiptRequestTest {
         validator.validate(receipt);
         assertThat(validator.validate(receipt), hasSize(1));
     }
-<<<<<<< HEAD
 
 
     @Test
@@ -52,6 +47,5 @@ public class CreateReceiptRequestTest {
         String tag = " ";
         assertThat(validator.validate(tag),hasSize(0));
     }
-=======
->>>>>>> b07c2f917ba21e931340a595ba355e5ac0b57f81
+
 }
